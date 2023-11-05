ea3d77cc135f538f7ca18ccb36a004627cd71304
Kenley Cheung <cheung.kenley@gmail.com>
2023-11-06 00:18:16 +1300

    Refactor item quality logic so that it is easier to follow

--- TypeScript/app/gilded-rose.ts
+++ TypeScript/app/gilded-rose.ts
@@ -3,6 +3,9 @@ const ITEM_AGED_BRIE = 'Aged Brie';
 const ITEM_BACKSTAGE_PASS = 'Backstage passes to a TAFKAL80ETC concert';
 const ITEM_CONJURED = 'Conjured Mana Cake';
 
+const MAX_QUALITY = 50;
+const MIN_QUALITY = 0;
+
 export class Item {
   name: string;
   sellIn: number;
@@ -24,41 +27,39 @@ export class GildedRose {
 
   updateQuality() {
     for (const item of this.items) {
-      this.updateItemQuality(item);
+      this.updateItem(item);
     }
 
     return this.items;
   }
 
-  updateItemQuality(item: Item) {
-    if (item.name != ITEM_AGED_BRIE && item.name != ITEM_BACKSTAGE_PASS) {
-      if (item.quality > 0) {
-        if (item.name != ITEM_SULFURAS) {
-          item.quality = item.quality - 1
+  // Resolves the new quality of the given item at the end of the day
+  resolveNewItemQuality(item: Item) {
+    switch (item.name) {
+      case ITEM_AGED_BRIE:
+        return Math.min(item.quality + 1, MAX_QUALITY);
+      case ITEM_BACKSTAGE_PASS:
+        if (item.sellIn <= 0) {
+          return 0;
+        } else if (item.sellIn <= 5) {
+          return Math.min(item.quality + 3, MAX_QUALITY);
+        } else if (item.sellIn <= 10) {
+          return Math.min(item.quality + 2, MAX_QUALITY);
+        } else {
+          return Math.min(item.quality + 1, MAX_QUALITY);
         }
-        if (item.name == ITEM_CONJURED) {
-          if (item.quality > 0) {
-            item.quality = item.quality - 1;
-          }
-        }
-      }
-    } else {
-      if (item.quality < 50) {
-        item.quality = item.quality + 1
-        if (item.name == ITEM_BACKSTAGE_PASS) {
-          if (item.sellIn < 11) {
-            if (item.quality < 50) {
-              item.quality = item.quality + 1
-            }
-          }
-          if (item.sellIn < 6) {
-            if (item.quality < 50) {
-              item.quality = item.quality + 1
-            }
-          }
-        }
-      }
+      case ITEM_SULFURAS:
+        return 80;
+      case ITEM_CONJURED:
+        return Math.max(item.quality - 2, MIN_QUALITY);
+      default:
+        return Math.max(item.quality - 1, MIN_QUALITY);
     }
+  }
+
+  // Update the given item's SellIn and quality
+  updateItem(item: Item) {
+    item.quality = this.resolveNewItemQuality(item);
     if (item.name != ITEM_SULFURAS) {
       item.sellIn = item.sellIn - 1;
     }
